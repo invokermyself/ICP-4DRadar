f24afad81549e113d84fbba6b82c6b66d8214644
invoker <invokermyself@163.com>
2023-06-13 17:07:05 +0800

    add depend packages && build OK

--- include/userdefine.h
+++ include/userdefine.h
@@ -1,7 +1,12 @@
 
-
+#include <sensor_msgs/Imu.h>
 #include <pcl/point_types.h>
+#include <Eigen/Core>
 
+
+typedef Eigen::Vector2d Vector2;
+typedef Eigen::Vector3d Vector3;
+typedef Eigen::Vector4d Vector4;
 typedef pcl::PointXYZI PointType;
 
 typedef struct 
@@ -22,3 +27,96 @@ typedef struct
     float arfa;
     float beta;
 }RadarPoint_Info2;
+
+struct ImuDataStamped
+{
+  ImuDataStamped() : dt{0} {}
+  ImuDataStamped(const ros::Time& time_stamp,
+                 const std::string frame_id,
+                 const double dt,
+                 const Vector3& a_b_ib,
+                 const Vector3& w_b_ib) :
+    time_stamp{time_stamp},
+    frame_id{frame_id},
+    dt{dt},
+    a_b_ib{a_b_ib},
+    w_b_ib{w_b_ib}
+  {
+  }
+
+  ImuDataStamped(const sensor_msgs::ImuConstPtr& imu_msg, const double dt) :
+    time_stamp{imu_msg->header.stamp},
+    frame_id{imu_msg->header.frame_id},
+    dt{dt},
+    a_b_ib{Vector3(imu_msg->linear_acceleration.x, imu_msg->linear_acceleration.y, imu_msg->linear_acceleration.z)},
+    w_b_ib{Vector3(imu_msg->angular_velocity.x, imu_msg->angular_velocity.y, imu_msg->angular_velocity.z)}
+  {
+  }
+
+  sensor_msgs::Imu toImuMsg()
+  {
+    sensor_msgs::Imu imu_msg;
+    imu_msg.header.stamp          = time_stamp;
+    imu_msg.angular_velocity.x    = w_b_ib.x();
+    imu_msg.angular_velocity.y    = w_b_ib.y();
+    imu_msg.angular_velocity.z    = w_b_ib.z();
+    imu_msg.linear_acceleration.x = a_b_ib.x();
+    imu_msg.linear_acceleration.y = a_b_ib.y();
+    imu_msg.linear_acceleration.z = a_b_ib.z();
+    return imu_msg;
+  }
+
+  ros::Time time_stamp;  // ros::Time
+  std::string frame_id;  // frame id
+  double dt;               // [s]
+  Vector3 a_b_ib;        // [m/s^2]
+  Vector3 w_b_ib;        // [rad/s]
+
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+
+struct RadarPointCloudType
+{
+  PCL_ADD_POINT4D;      // position in [m]
+  float snr_db;         // CFAR cell to side noise ratio in [dB]
+  float v_doppler_mps;  // Doppler velocity in [m/s]
+  float noise_db;       // CFAR noise level of the side of the detected cell in [dB]
+  float range;          // range in [m]
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+
+struct mmWaveCloudType
+{
+  PCL_ADD_POINT4D;
+  union
+  {
+    struct
+    {
+      float intensity;
+      float velocity;
+    };
+    float data_c[4];
+  };
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+
+
+struct OculiiPointCloudType
+{
+  PCL_ADD_POINT4D;      // position in [m]
+  float Doppler;  // Doppler velocity in [m/s]
+  float Range;          // range in [m]
+  float Power;
+  float Alpha;
+  float Beta;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+
+struct ColoRadarPointCloudType
+{
+  PCL_ADD_POINT4D;      // position in [m]
+  float intensity;  
+  float range;          // range in [m]
+  float doppler;  // Doppler velocity in [m/s]
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
--- src/radar_odometry.cpp
+++ src/radar_odometry.cpp
@@ -15,10 +15,12 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <ros/ros.h>
 #include <rosbag/bag.h>
+#include <rosbag/view.h>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #include <Eigen/Dense>
 #include <boost/thread/thread.hpp>
+#include <mutex>
 
 #include <pcl/pcl_macros.h>
 #include "userdefine.h"
@@ -26,10 +28,49 @@
 #include "tic_toc.h"
 #include "ikd_Tree.h"
 #include "radar_ego_velocity_estimator/radar_ego_velocity_estimator.h"
+#include "fast_gicp/gicp/fast_gicp.hpp"
 
 #define MAX_SEARCH_RADIUS 2.0f
 
 using namespace std;
+
+// clang-format off
+POINT_CLOUD_REGISTER_POINT_STRUCT(RadarPointCloudType,
+                                  (float, x, x)
+                                  (float, y, y)
+                                  (float, z, z)
+                                  (float, snr_db, snr_db)
+                                  (float, noise_db,   noise_db)
+                                  (float, v_doppler_mps,   v_doppler_mps)
+                                  )
+
+
+POINT_CLOUD_REGISTER_POINT_STRUCT (mmWaveCloudType,
+                                    (float, x, x)
+                                    (float, y, y)
+                                    (float, z, z)
+                                    (float, intensity, intensity)
+                                    (float, velocity, velocity))
+
+POINT_CLOUD_REGISTER_POINT_STRUCT (OculiiPointCloudType,
+                                    (float, x, x)
+                                    (float, y, y)
+                                    (float, z, z)
+                                    (float, Doppler, Doppler)
+                                    (float, Range, Range)
+                                    (float, Power, Power)
+                                    (float, Alpha, Alpha)
+                                    (float, Beta, Beta))
+
+POINT_CLOUD_REGISTER_POINT_STRUCT (ColoRadarPointCloudType,
+                                    (float, x, x)
+                                    (float, y, y)
+                                    (float, z, z)
+                                    (float, intensity, intensity)
+                                    (float, range, range)
+                                    (float, doppler, doppler))
+
+// clang-format on
 
 double para_q[4] = {0, 0, 0, 1};
 double para_t[3] = {0, 0, 0};
@@ -50,196 +91,292 @@ Eigen::Vector3d t_w_curr(0, 0, 0);
 Eigen::Matrix<double, 3, 3> Rtrans;
 Eigen::MatrixXd currOdom;
 
+bool initialed = false;
+std::mutex mutex_1;
+
+std::queue<ImuDataStamped> queue_imu;
+std::queue<sensor_msgs::PointCloud2> queue_radar;
+ImuDataStamped last_imu;
+ImuDataStamped imu_data;
+
 void pointAssociateToMap(PointType const *const pi, PointType *const po)
 {
-	Eigen::Vector3d point_curr(pi->x, pi->y, pi->z);
-	Eigen::Vector3d point_w = Rtrans * point_curr + t;
-	po->x = point_w.x();
-	po->y = point_w.y();
-	po->z = point_w.z();
-	po->intensity = pi->intensity;
+  Eigen::Vector3d point_curr(pi->x, pi->y, pi->z);
+  Eigen::Vector3d point_w = Rtrans * point_curr + t;
+  po->x = point_w.x();
+  po->y = point_w.y();
+  po->z = point_w.z();
+  po->intensity = pi->intensity;
 }
 
 void pointAssociateToSubMap(PointType const *const pi, PointType *const po, Eigen::Matrix<double, 4, 4> Trans)
 {
-	Eigen::Vector4d point_curr(pi->x, pi->y, pi->z, 1.0);
-	Eigen::Vector4d point_w = Trans.inverse() * point_curr;
-	po->x = point_w.x();
-	po->y = point_w.y();
-	po->z = point_w.z();
-	po->intensity = pi->intensity;
+  Eigen::Vector4d point_curr(pi->x, pi->y, pi->z, 1.0);
+  Eigen::Vector4d point_w = Trans.inverse() * point_curr;
+  po->x = point_w.x();
+  po->y = point_w.y();
+  po->z = point_w.z();
+  po->intensity = pi->intensity;
 }
 
 int get_file_lines(const std::string file_path)
 {
-	char c;
-	int line = 0;
-	if (boost::filesystem::exists(file_path))
-	{
-		std::ifstream file(file_path, std::ifstream::in);
-		while (file.get(c))
-		{
-			if (c == '\n')
-				line++;
-		}
-	}
-	else
-	{
-		std::cout << "wrong file path !" << endl;
-	}
-	return line;
+  char c;
+  int line = 0;
+  if (boost::filesystem::exists(file_path))
+  {
+    std::ifstream file(file_path, std::ifstream::in);
+    while (file.get(c))
+    {
+      if (c == '\n')
+        line++;
+    }
+  }
+  else
+  {
+    std::cout << "wrong file path !" << endl;
+  }
+  return line;
 }
 
 std::vector<float> read_radar_featurePoint(const std::string radar_data_path)
 {
-	if (boost::filesystem::exists(radar_data_path))
-	{
-		std::string value;
-		std::vector<float> radar_data_buffer;
-		int lines = get_file_lines(radar_data_path);
-		std::ifstream radar_data_file(radar_data_path, std::ifstream::in);
+  if (boost::filesystem::exists(radar_data_path))
+  {
+    std::string value;
+    std::vector<float> radar_data_buffer;
+    int lines = get_file_lines(radar_data_path);
+    std::ifstream radar_data_file(radar_data_path, std::ifstream::in);
 
-		for (int i = 0; i < lines; i++)
-		{
-			for (int j = 0; j < 2; j++)
-			{
-				getline(radar_data_file, value, ' ');
-				radar_data_buffer.push_back(atof(value.c_str()));
-			}
-			getline(radar_data_file, value, '\n'); // 换行
-			radar_data_buffer.push_back(atof(value.c_str()));
-		}
-		return radar_data_buffer;
-	}
-	else
-	{
-		std::vector<float> radar_data_buffer(0);
-		return radar_data_buffer;
-	}
+    for (int i = 0; i < lines; i++)
+    {
+      for (int j = 0; j < 2; j++)
+      {
+        getline(radar_data_file, value, ' ');
+        radar_data_buffer.push_back(atof(value.c_str()));
+      }
+      getline(radar_data_file, value, '\n'); // 换行
+      radar_data_buffer.push_back(atof(value.c_str()));
+    }
+    return radar_data_buffer;
+  }
+  else
+  {
+    std::vector<float> radar_data_buffer(0);
+    return radar_data_buffer;
+  }
 }
+
+void callbackIMU(const sensor_msgs::ImuConstPtr &imu_msgs);
+void callbackRadarScan(const sensor_msgs::PointCloud2ConstPtr &radar_msg);
+bool pcl2msgToPcl(const sensor_msgs::PointCloud2 &pcl_msg, pcl::PointCloud<RadarPointCloudType> &scan);
+void main_task();
 
 int main(int argc, char **argv)
 {
 
-	ros::init(argc, argv, "radar_odometry");
-	ros::NodeHandle n("~");
-	std::size_t order = 1;
+  ros::init(argc, argv, "radar_odometry");
+  ros::NodeHandle n("~");
+  std::size_t order = 1;
 
-	std::string dataset_folder;
-	n.getParam("dataset_folder", dataset_folder);
+  std::string bag_path;
+  std::string topic_imu;
+  std::string topic_radar_scan;
+  n.getParam("bag_path", bag_path);
+  n.getParam("topic_imu", topic_imu);
+  n.getParam("topic_radar_scan", topic_radar_scan);
 
-	pcl::PointCloud<pcl::PointXYZI>::Ptr featurePointsLast(new pcl::PointCloud<pcl::PointXYZI>);
-	pcl::PointCloud<pcl::PointXYZI>::Ptr featurePointsCurr(new pcl::PointCloud<pcl::PointXYZI>);
-	pcl::PointCloud<pcl::PointXYZI>::Ptr Final(new pcl::PointCloud<pcl::PointXYZI>);
+  ros::Rate r(100.0);
 
-	std::stringstream radar_odom_path;
-	radar_odom_path << "/home/invoker/catkin_ws/src/ICP-4DRadar/radar_odometry.txt";
-	std::ofstream odom(radar_odom_path.str(), std::ios::trunc);
-	odom.setf(std::ios::dec, std::ios::floatfield);
-	odom.precision(15);
+  // read bag
+  rosbag::Bag source_bag;
+  source_bag.open(bag_path, rosbag::bagmode::Read);
+  std::vector<std::string> topics;
+  topics.push_back(topic_imu);
+  topics.push_back(topic_radar_scan);
 
-	ros::Rate r(100.0);
+  rosbag::View view(source_bag, rosbag::TopicQuery(topics));
 
-	while (ros::ok())
-	{
-		// ceres::LossFunction *loss_function = NULL;
-		std::stringstream radar_featurelast_path;
-		std::stringstream radar_featurecurr_path;
+  for (const rosbag::MessageInstance &m : view)
+  {
+    const auto topic = m.getTopic();
+    if (topic == topic_imu)
+    {
+      const auto imu_msg_bag = m.instantiate<sensor_msgs::Imu>();
+      if (imu_msg_bag != NULL)
+      {
+        callbackIMU(imu_msg_bag);
+      }
+    }
 
-		radar_featurelast_path << dataset_folder << std::setfill('0') << std::setw(4) << order << "-1.txt";
-		radar_featurecurr_path << dataset_folder << std::setfill('0') << std::setw(4) << (order + 1) << "-1.txt";
+    else if (topic == topic_radar_scan)
+    {
+      const auto radar_scan = m.instantiate<sensor_msgs::PointCloud2>();
+      if (radar_scan != NULL)
+        callbackRadarScan(radar_scan);
+    }
 
-		std::vector<float> radarlast_data = read_radar_featurePoint(radar_featurelast_path.str());
-		std::vector<float> radarcurr_data = read_radar_featurePoint(radar_featurecurr_path.str());
-		ceres::LossFunction *loss_function = new ceres::HuberLoss(0.1);
-		ceres::LocalParameterization *q_parameterization =
-			new ceres::EigenQuaternionParameterization();
-		ceres::Problem::Options problem_options;
+    ros::spinOnce();
 
-		ceres::Problem problem(problem_options);
-		problem.AddParameterBlock(para_q, 4, q_parameterization);
-		problem.AddParameterBlock(para_t, 3);
+    main_task();
+  }
+}
 
-		pcl::PointXYZI pointSel;
-		std::vector<int> pointSearchInd;
-		std::vector<float> pointSearchSqDis;
-		featurePointsLast->clear();
-		featurePointsCurr->clear();
+void main_task()
+{
+  // GICP
 
-		for (std::size_t i = 0; i < radarlast_data.size(); i += 3)
-		{
-			pcl::PointXYZI point;
-			point.x = radarlast_data[i];
-			point.y = radarlast_data[i + 1];
-			point.z = radarlast_data[i + 2];
-			featurePointsLast->push_back(point);
-		}
+  pcl::PointCloud<pcl::PointXYZI>::Ptr src(new pcl::PointCloud<pcl::PointXYZI>);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr tar(new pcl::PointCloud<pcl::PointXYZI>);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr Final(new pcl::PointCloud<pcl::PointXYZI>);
+  pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp;
+  // icp.setMaximumIterations(200);
 
-		for (std::size_t i = 0; i < radarcurr_data.size(); i += 3)
-		{
-			pcl::PointXYZI point;
-			point.x = radarcurr_data[i];
-			point.y = radarcurr_data[i + 1];
-			point.z = radarcurr_data[i + 2];
-			featurePointsCurr->push_back(point);
-		}
+  if (!initialed)
+  {
+    icp.setInputTarget(tar);
+    initialed = true;
+  }
+  icp.align(*Final);
 
+  std::cout << "has converged:" << icp.hasConverged() << " score: " << icp.getFitnessScore() << std::endl;
 
-		pcl::IterativeClosestPoint<pcl::PointXYZI, pcl::PointXYZI> icp;
-		icp.setInputSource(featurePointsCurr);
-		icp.setInputTarget(featurePointsLast);
-		// icp.setMaximumIterations(200);
-		icp.align(*Final);
+  // output the transformation matrix
+  std::cout << icp.getFinalTransformation() << std::endl;
+  double score = icp.getFitnessScore();
+  Eigen::Matrix<double, 4, 4> icp_result = icp.getFinalTransformation().cast<double>();
 
-		std::cout << "has converged:" << icp.hasConverged() << " score: " << icp.getFitnessScore() << std::endl;
+  t_w_curr = t_w_curr + q_w_curr * t_last_curr;
+  q_w_curr = q_w_curr * q_last_curr;
+}
 
-		// output the transformation matrix
-		std::cout << icp.getFinalTransformation() << std::endl;
-		double score = icp.getFitnessScore();
-		Eigen::Matrix<double, 4, 4> icp_result = icp.getFinalTransformation().cast<double>();
+void callbackIMU(const sensor_msgs::ImuConstPtr &imu_msg)
+{
+  mutex_1.lock();
+  double dt = 0.01;
+  if (std::fabs(last_imu.dt) > 1.0e-6)
+    dt = (imu_msg->header.stamp - last_imu.time_stamp).toSec();
+  last_imu = ImuDataStamped(imu_msg, dt);
+  queue_imu.push(last_imu);
+  mutex_1.unlock();
+}
 
-		kdtreeEdgeFeatureLast->setInputCloud(featurePointsLast);
+void callbackRadarScan(const sensor_msgs::PointCloud2ConstPtr &radar_msg)
+{
+  mutex_1.lock();
+  queue_radar.push(*radar_msg);
+  mutex_1.unlock();
+}
 
-		for (int i = 0; i < featurePointsCurr->size(); ++i)
-		{
-			int closestPointInd = -1, minPointInd2 = -1;
-			pointSel = featurePointsCurr->points[i];
-			kdtreeEdgeFeatureLast->nearestKSearch(pointSel, 2, pointSearchInd, pointSearchSqDis);
-			if (*max_element(pointSearchSqDis.begin(), pointSearchSqDis.end()) < MAX_SEARCH_RADIUS)
-			{
-				Eigen::Vector3d curr_point(featurePointsCurr->points[i].x,
-										   featurePointsCurr->points[i].y,
-										   featurePointsCurr->points[i].z);
-				Eigen::Vector3d last_point_a(featurePointsLast->points[pointSearchInd[0]].x,
-											 featurePointsLast->points[pointSearchInd[0]].y,
-											 featurePointsLast->points[pointSearchInd[0]].z);
-				Eigen::Vector3d last_point_b(featurePointsLast->points[pointSearchInd[1]].x,
-											 featurePointsLast->points[pointSearchInd[1]].y,
-											 featurePointsLast->points[pointSearchInd[1]].z);
+bool pcl2msgToPcl(const sensor_msgs::PointCloud2 &pcl_msg, pcl::PointCloud<RadarPointCloudType> &scan)
+{
+  // TODO: add support for ti_mmwave_rospkg clound type
 
-				double s = 1.0;
-				ceres::CostFunction *cost_function = RadarEdgeFactor::Create(curr_point, last_point_a, last_point_b, s);
-				problem.AddResidualBlock(cost_function, loss_function, para_q, para_t);
-			}
-		}
+  std::set<std::string> fields;
+  std::string fields_str = "";
 
-		TicToc t_solver;
-		ceres::Solver::Options options;
-		options.linear_solver_type = ceres::DENSE_QR;
-		options.max_num_iterations = 4;
-		options.minimizer_progress_to_stdout = false;
-		ceres::Solver::Summary summary;
-		ceres::Solve(options, &problem, &summary);
-		printf("solver time %f ms \n", t_solver.toc());
+  for (const auto &field : pcl_msg.fields)
+  {
+    fields.emplace(field.name);
+    fields_str += field.name + ", ";
+  }
 
-		t_w_curr = t_w_curr + q_w_curr * t_last_curr;
-		q_w_curr = q_w_curr * q_last_curr;
-		odom << t_w_curr(0) << " "
-			 << t_w_curr(1) << " "
-			 << t_w_curr(2) << std::endl;
+  if (fields.find("x") != fields.end() && fields.find("y") != fields.end() && fields.find("z") != fields.end() &&
+      fields.find("snr_db") != fields.end() && fields.find("noise_db") != fields.end() &&
+      fields.find("v_doppler_mps") != fields.end())
+  {
+    ROS_INFO_ONCE("[pcl2msgToPcl]: Detected rio pcl format!");
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(pcl_msg, pcl_pc2);
+    pcl::fromPCLPointCloud2(pcl_pc2, scan);
 
-		order++;
-		r.sleep();
-	}
-	odom.close();
+    // fix format
+    for (auto &p : scan)
+      p.range = p.getVector3fMap().norm();
+
+    return true;
+  }
+  else if (fields.find("x") != fields.end() && fields.find("y") != fields.end() && fields.find("z") != fields.end() &&
+           fields.find("intensity") != fields.end() && fields.find("velocity") != fields.end())
+  {
+    ROS_INFO_ONCE("[pcl2msgToPcl]: Detected ti_mmwave_rospkg pcl format!");
+
+    pcl::PointCloud<mmWaveCloudType> scan_mmwave;
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(pcl_msg, pcl_pc2);
+    pcl::fromPCLPointCloud2(pcl_pc2, scan_mmwave);
+
+    scan.clear();
+    for (const auto &p : scan_mmwave)
+    {
+      RadarPointCloudType p_;
+      p_.x = -p.y;
+      p_.y = p.x;
+      p_.z = p.z;
+      p_.snr_db = p.intensity;
+      p_.v_doppler_mps = p.velocity;
+      p_.range = p.getVector3fMap().norm();
+      p_.noise_db = -1.;
+      scan.push_back(p_);
+    }
+    return true;
+  }
+  else if (fields.find("x") != fields.end() && fields.find("y") != fields.end() && fields.find("z") != fields.end() &&
+           fields.find("Doppler") != fields.end() && fields.find("Range") != fields.end() && fields.find("Power") != fields.end() && fields.find("Alpha") != fields.end() && fields.find("Beta") != fields.end())
+  {
+    ROS_INFO_ONCE("[pcl2msgToPcl]: Detected oculii pcl format!");
+
+    pcl::PointCloud<OculiiPointCloudType> scan_oculii;
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(pcl_msg, pcl_pc2);
+    pcl::fromPCLPointCloud2(pcl_pc2, scan_oculii);
+
+    scan.clear();
+    for (const auto &p : scan_oculii)
+    {
+      RadarPointCloudType p_;
+      p_.x = p.x;
+      p_.y = p.y;
+      p_.z = p.z;
+      p_.snr_db = p.Power;
+      p_.v_doppler_mps = p.Doppler;
+      p_.range = p.Range;
+      p_.noise_db = -1.;
+      scan.push_back(p_);
+    }
+    return true;
+  }
+
+  else if (fields.find("x") != fields.end() && fields.find("y") != fields.end() && fields.find("z") != fields.end() &&
+           fields.find("intensity") != fields.end() && fields.find("range") != fields.end() && fields.find("doppler") != fields.end())
+  {
+    ROS_INFO_ONCE("[pcl2msgToPcl]: Detected ColoRadar pcl format!");
+
+    pcl::PointCloud<ColoRadarPointCloudType> scan_ColoRadar;
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(pcl_msg, pcl_pc2);
+    pcl::fromPCLPointCloud2(pcl_pc2, scan_ColoRadar);
+
+    scan.clear();
+    for (const auto &p : scan_ColoRadar)
+    {
+      RadarPointCloudType p_;
+      p_.x = p.x;
+      p_.y = p.y;
+      p_.z = p.z;
+      p_.snr_db = p.intensity;
+      p_.v_doppler_mps = p.doppler;
+      p_.range = p.range;
+      p_.noise_db = -1.;
+      scan.push_back(p_);
+    }
+    return true;
+  }
+
+  else
+  {
+    ROS_ERROR_STREAM(
+        "[pcl2msgToPcl]: Unsupported point cloud with fields: " << fields_str.substr(0, fields_str.size() - 2));
+    return false;
+  }
 }
